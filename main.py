dd6b3775c8f3abbbf9784d8fbacf32a1b0e1cf52
Alexey917 <pochtovii.1996@mail.ru>
2026-07-16 14:01:00 +0300

    added new words

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ list_id = []
 right_answers = {}
 
 while True:
-    number = random.randint(1, 1038)
+    number = random.randint(1, 1053)
     if number not in list_id:
         list_id.append(number)
     if len(list_id) == 30:
